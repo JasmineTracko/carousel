eef4c570ab0406f0ee1ed2ac943489ecb8758984
JasmineAesys <jasmine.cipollone@aesys.tech>
2024-10-04 22:52:04 +0200

    mocks: data for carousel

---
+++ src/mock/carouselData.ts
@@ -0,0 +1,104 @@
+export const carouselData = [
+  {
+    year: 2010,
+    description: "Acquisition of <strong>Matteo Artista</strong>",
+    companies: [{ id: "a1222", name: "Matteo Artista" }],
+  },
+  {
+    year: 2011,
+    description: "Acquisition of <strong>Gruppo Chiken</strong>",
+    companies: [{ id: "a1223", name: "Gruppo Chiken" }],
+  },
+  {
+    year: 2012,
+    description:
+      "Acquisition of <strong>Pongo Group</strong> Broking: Acquisition of <strong>Jama Trade</strong>",
+    companies: [
+      { id: "a1224", name: "Pongo Group Broking" },
+      { id: "a1225", name: "Jama Trade" },
+    ],
+  },
+  {
+    year: 2013,
+    description:
+      "Acquisition of <strong>Franco Consulting</strong> and the activties of <strong>Master of Puppets</strong>",
+    companies: [
+      { id: "a1226", name: "Franco Consulting" },
+      { id: "a1227", name: "Master of Puppets" },
+    ],
+  },
+  {
+    year: 2014,
+    description:
+      "Acquisition of <strong>Whitesnake</strong> and <strong>ACDC</strong>",
+    companies: [
+      { id: "a1227", name: "Whitesnake" },
+      { id: "a1228", name: "ACDC" },
+    ],
+  },
+  {
+    year: 2015,
+    description: "Acquisition of <strong>Matteo Artista</strong>",
+    companies: [{ id: "a1229", name: "Matteo Artista" }],
+  },
+  {
+    year: 2016,
+    description: "Acquisition of <strong>Gruppo Chiken</strong>",
+    companies: [{ id: "a1230", name: "Gruppo Chiken" }],
+  },
+  {
+    year: 2017,
+    description:
+      "Acquisition of <strong>Pongo Group</strong> Broking: Acquisition of <strong>Jama Trade</strong>",
+    companies: [
+      { id: "a1231", name: "Pongo Group Broking" },
+      { id: "a1232", name: "Jama Trade" },
+    ],
+  },
+  {
+    year: 2018,
+    description:
+      "Acquisition of <strong>Franco Consulting</strong> and the activties of <strong>Master of Puppets</strong>",
+    companies: [
+      { id: "a1233", name: "Franco Consulting" },
+      { id: "a1234", name: "Master of Puppets" },
+    ],
+  },
+  {
+    year: 2019,
+    description: "Acquisition of <strong>Matteo Artista</strong>",
+    companies: [{ id: "a1235", name: "Matteo Artista" }],
+  },
+  {
+    year: 2020,
+    description: "Acquisition of <strong>Gruppo Chiken</strong>",
+    companies: [{ id: "a1236", name: "Gruppo Chiken" }],
+  },
+  {
+    year: 2021,
+    description:
+      "Acquisition of <strong>Pongo Group</strong> Broking: Acquisition of <strong>Jama Trade</strong>",
+    companies: [
+      { id: "a1237", name: "Pongo Group Broking" },
+      { id: "a1238", name: "Jama Trade" },
+    ],
+  },
+  {
+    year: 2022,
+    description:
+      "Acquisition of <strong>Franco Consulting</strong> and the activties of <strong>Master of Puppets</strong>",
+    companies: [
+      { id: "a1239", name: "Franco Consulting" },
+      { id: "a1240", name: "Master of Puppets" },
+    ],
+  },
+  {
+    year: 2023,
+    description:
+      "Acquisition of <strong>Whitesnake</strong> and <strong>ACDC</strong>",
+    companies: [
+      { id: "a1241", name: "Whitesnake" },
+      { id: "a1242", name: "ACDC" },
+    ],
+  },
+];
